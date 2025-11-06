a6ec71d3480bd6c0d69cc01b49c62cfa4868d865
Sushrut Shastri <shsushrut31@gmail.com>
2025-11-05 23:40:08 -0700

    fixed video control buttons UI

--- app/VideoPlayer/Views/VideoControlsOverlay.swift
+++ app/VideoPlayer/Views/VideoControlsOverlay.swift
@@ -15,44 +15,53 @@ struct VideoControlsOverlay: View {
             Button {
                 viewModel.playPrevious()
             } label: {
-                Image("previous")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(viewModel.canGoPrevious ? .black : .gray)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(10)
-                    .frame(width: 50, height: 50)
-                    .background(Circle().fill(Color.gray.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
+                MediaControlIcon(
+                    name: "previous",
+                    isEnabled: viewModel.canGoPrevious
+                )
+                .frame(width: 50, height: 50)
+                .background(Circle().fill(Color.white.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
             }
             .disabled(!viewModel.canGoPrevious)
             
             Button {
                 viewModel.togglePlayPause()
             } label: {
-                Image(viewModel.isPlaying ? "pause" : "play")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(.black)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(10)
-                    .frame(width: 70, height: 70)
-                    .background(Circle().fill(Color.gray.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
+                MediaControlIcon(
+                    name: viewModel.isPlaying ? "pause" : "play",
+                    isEnabled: true
+                )
+                .frame(width: 70, height: 70)
+                .background(Circle().fill(Color.white.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
             }
             
             Button {
                 viewModel.playNext()
             } label: {
-                Image("next")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(viewModel.canGoNext ? .black : .gray)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(10)
-                    .frame(width: 50, height: 50)
-                    .background(Circle().fill(Color.gray.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
+                MediaControlIcon(
+                    name: "next",
+                    isEnabled: viewModel.canGoNext
+                )
+                .frame(width: 50, height: 50)
+                .background(Circle().fill(Color.white.opacity(0.7)).stroke(.gray, style: StrokeStyle(lineWidth: 1.0)))
             }
             .disabled(!viewModel.canGoNext)
         }
+    }
+}
+
+struct MediaControlIcon: View {
+    let name: String
+    let isEnabled: Bool
+    
+    var body: some View {
+        Image(name)
+            .resizable()
+            .renderingMode(.template)
+            .foregroundColor(isEnabled ? .black : .gray)
+            .aspectRatio(contentMode: .fit)
+            .padding(10)
+        
     }
 }
 
