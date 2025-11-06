e4ace9066469d58b0691b736781e5d79e6f904dc
Sushrut Shastri <shsushrut31@gmail.com>
2025-11-05 19:34:20 -0700

    Added response data models

---
+++ app/VideoPlayer/Models/Video.swift
@@ -0,0 +1,29 @@
+//
+//  Video.swift
+//  VideoPlayer
+//
+//  Created by Sushrut Shastri on 2025-11-05.
+//
+
+import Foundation
+
+struct Video: Identifiable, Codable {
+    let id: String
+    let title: String
+    let hlsURL: String
+    let fullURL: String
+    let description: String
+    let publishedAt: Date
+    let author: Author
+}
+
+struct Author: Identifiable, Codable {
+    let id: String
+    let name: String
+}
+
+extension Video {
+    static func sortedByDate(_ videos: [Video]) -> [Video] {
+        videos.sorted { $0.publishedAt > $1.publishedAt }
+    }
+}
---
+++ app/VideoPlayer/Models/VideoResponse.swift
@@ -0,0 +1,10 @@
+//
+//  VideoResponse.swift
+//  VideoPlayer
+//
+//  Created by Sushrut Shastri on 2025-11-05.
+//
+
+import Foundation
+
+typealias VideoResponse = [Video]
